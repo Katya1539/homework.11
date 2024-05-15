71aef21fe319d2f35058a499393ace20b9aded21
Katya1539 <erkenovakatya@mail.ru>
2024-05-15 23:22:14 +0300

    Reformated code

--- src/main/java/ru/netology/MovieManager.java
+++ src/main/java/ru/netology/MovieManager.java
@@ -5,11 +5,11 @@ public class MovieManager {
     private int limit;
 
     public MovieManager() {
-    this.limit = 5;
+        this.limit = 5;
     }
 
     public MovieManager(int limit) {
-    this.limit = limit;
+        this.limit = limit;
     }
 
     public void addMovie(String movie) {
@@ -25,17 +25,17 @@ public class MovieManager {
         return movies;
     }
 
-   public String[] findLast() {
+    public String[] findLast() {
         int resultLength;
         if (movies.length < limit) {
             resultLength = movies.length;
         } else {
             resultLength = limit;
         }
-    String[] tmp = new String[resultLength];
-       for (int i = 0; i < tmp.length; i++) {
-           tmp[i] = movies[movies.length - 1 -i];
-       }
-       return tmp;
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return tmp;
     }
 }
